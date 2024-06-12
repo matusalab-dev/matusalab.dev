6241cfddbffc183a099612258aac0cd08dda7f7b
matusala <mtslabrh97@gmail.com>
2024-06-12 17:55:48 +0300

    move all certs data into constants foolder

--- app/components/Certifications.tsx
+++ app/components/Certifications.tsx
@@ -1,35 +1,4 @@
-const CERTIFICATIONS = [
-  {
-    course: "Introduction to Front-end development",
-    offeredBy: "Meta",
-    startDate: "2023",
-    endDate: "current",
-  },
-  {
-    course: "Principles of UI/UX Design",
-    offeredBy: "Meta",
-    startDate: "2023",
-    endDate: "current",
-  },
-  {
-    course: "HTML & CSS",
-    offeredBy: "Meta",
-    startDate: "2023",
-    endDate: "current",
-  },
-  {
-    course: "react advanced",
-    offeredBy: "Meta",
-    startDate: "2023",
-    endDate: "current",
-  },
-  {
-    course: "Javascript",
-    offeredBy: "Meta",
-    startDate: "2023",
-    endDate: "current",
-  },
-];
+import { CERTIFICATIONS } from "../constants/certifications";
 
 const Certifications = () => {
   return (
---
+++ app/constants/certifications.tsx
@@ -0,0 +1,32 @@
+export const CERTIFICATIONS = [
+  {
+    course: "Introduction to Front-end development",
+    offeredBy: "Meta",
+    startDate: "2023",
+    endDate: "current",
+  },
+  {
+    course: "Principles of UI/UX Design",
+    offeredBy: "Meta",
+    startDate: "2023",
+    endDate: "current",
+  },
+  {
+    course: "HTML & CSS",
+    offeredBy: "Meta",
+    startDate: "2023",
+    endDate: "current",
+  },
+  {
+    course: "react advanced",
+    offeredBy: "Meta",
+    startDate: "2023",
+    endDate: "current",
+  },
+  {
+    course: "Javascript",
+    offeredBy: "Meta",
+    startDate: "2023",
+    endDate: "current",
+  },
+];
