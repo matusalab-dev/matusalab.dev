ba50b958a415da8613f242bcfd6264ef1948b6c3
matusala <mtslabrh97@gmail.com>
2024-07-21 09:33:09 +0300

    add information into the projects section

--- app/constants/projects.tsx
+++ app/constants/projects.tsx
@@ -17,6 +17,14 @@ export const PROJECT_INFO: ProjectInfoType[] = [
     className: "lg:col-[1/5]",
 
     liveUrl: "https://room-store-two.vercel.app/",
+    overview:
+      "The room Project is an e-commerce platform designed to streamline furniture sales, providing a user-friendly interface for browsing and purchasing furniture. It was created to address the challenge of managing diverse product categories and enhancing the online shopping experience for wholesalers with multiple sales branches within the country with features like Product category section hover add-to-cart, detailed product pages, and easy checkout.",
+    techStacks: [
+      "React",
+      "React-Context-Api",
+      "React-Hooks",
+      "React-Router-Dom-V-6",
+    ],
   },
   {
     title: "little-lemon",
@@ -24,7 +32,16 @@ export const PROJECT_INFO: ProjectInfoType[] = [
     description: "restaurant table reservation",
     type: "web app",
     className: "md:col-[1/-1] lg:col-[5/-1]",
-    liveUrl: "https://www.room-ecommerce.pages.dev",
+    liveUrl: "https://little-lemon-eosin.vercel.app/",
+    overview:
+      "Little-Lemon Table Reservation Web App is a user-friendly solution designed to streamline the dining experience. Users can effortlessly browse available tables, select preferred time slots, and make reservations with ease.",
+    techStacks: [
+      "React",
+      "React-Context-Api",
+      "React-Hooks",
+      "React-Router-Dom-V-6",
+      "Formik Library",
+    ],
   },
   {
     title: "loopstudios",
@@ -32,15 +49,38 @@ export const PROJECT_INFO: ProjectInfoType[] = [
     description: "eye catching marketing landing page",
     type: "landing page",
     className: "lg:col-[3/-1]",
-    liveUrl: "https://www.room-ecommerce.pages.dev",
+    liveUrl: "https://loopstudios-theta-sable.vercel.app/",
+    overview:
+      "The primary goal of this project is to develop a compelling and visually appealing digital presence, modern, user-friendly and responsive website that effectively showcase.",
+    techStacks: [
+      "HTML 5",
+      "CSS 3",
+      "CSS GRID Layout",
+      "Flex Box Layout",
+      "CSS Varibles",
+      "JAVASCRIPT",
+    ],
   },
+
   {
     title: "sunnyside",
     imgSrc: "/mockup/sunnyside-desktop-preview.jpg",
     description: "high converting landing page",
     type: "landing page",
     className: "lg:col-[1/5]",
-    liveUrl: "https://www.room-ecommerce.pages.dev",
+    liveUrl: "https://sunnyside-jade.vercel.app/",
+    overview:
+      "The primary goal of this project is to develop a compelling and visually appealing digital presence, modern, user-friendly and responsive website that effectively showcase.",
+    techStacks: [
+      "HTML 5",
+      "CSS 3",
+      "SASS",
+      "BEM Methodology",
+      "CSS GRID Layout",
+      "Flex Box Layout",
+      "CSS Varibles",
+      "JAVASCRIPT",
+    ],
   },
 
   // {
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 import NavBar from "@/app/components/nav/NavBar";
 import { NAV_INFO_HOME } from "@/app/constants/navbar";
 import { ArrowVector } from "@/public/icons/ArrowVector";
+import { PROJECT_INFO } from "@/app/constants/projects";
 
 interface PageProps {
   params: {
@@ -47,39 +48,54 @@ const Projects: FC<PageProps> = ({ params, searchParams }) => {
             />
             Back to all projects
           </Link>
-          <div className="flex flex-row justify-between items-center gap-5 sm:items-center">
-            <h1 className="capitalize font-medium text-4xl xs:text-5xl sm:font-extrabold">
-              {slug}
-            </h1>
-            <a
-              href="https://room-store-two.vercel.app/"
-              target="_blank"
-              // onClick={goToPortfolio}
-              title="live site"
-              className="text-[1rem] text-white w-10 h-10 xs:w-[5.75rem] xs:h-[5.75rem] sm:w-24 sm:h-24 flex justify-center items-center gap-2 rounded-full bg-black"
-            >
-              <span className="hidden xs:inline">Live site</span>
-              <ArrowVector color="white" width="12" height="12" />
-            </a>
-          </div>
-          <div className="flex flex-col space-y-3">
-            <h2 className="capitalize text-2xl sm:text-4xl">Overview</h2>
-            <p className=" max-w-[45ch] text-lg ">
-              The primary goal of this project is to develop a compelling and
-              visually appealing digital presence, modern, user-friendly and
-              responsive website that effectively showcase.
-            </p>
-          </div>
-          <div className="flex flex-col space-y-3">
-            <h3 className="capitalize text-2xl sm:text-4xl">Tech-stacks</h3>
-            <ul className="flex gap-2 flex-wrap text-white text-[1rem]">
-              <li className="bg-black px-2 h-6 flex items-center">React</li>
-              <li className="bg-black px-2 h-6 flex items-center">Formik</li>
-              <li className="bg-black px-2 h-6 flex items-center">
-                React-router
-              </li>
-            </ul>
-          </div>
+          {PROJECT_INFO.map((project) => {
+            if (project.title === slug) {
+              return (
+                <>
+                  <div className="flex flex-row justify-between items-center gap-5 sm:items-center">
+                    <h1 className="capitalize font-medium text-4xl xs:text-5xl sm:font-extrabold">
+                      {slug}
+                    </h1>
+                    <a
+                      href={project.liveUrl}
+                      target="_blank"
+                      // onClick={goToPortfolio}
+                      title="live site"
+                      className="text-[1rem] text-white w-10 h-10 xs:w-[5.75rem] xs:h-[5.75rem] sm:w-24 sm:h-24 flex justify-center items-center gap-2 rounded-full bg-black"
+                    >
+                      <span className="hidden xs:inline">Live site</span>
+                      <ArrowVector color="white" width="12" height="12" />
+                    </a>
+                  </div>
+                  <div className="flex flex-col space-y-3">
+                    <h2 className="capitalize text-2xl sm:text-4xl">
+                      Overview
+                    </h2>
+                    <p className=" max-w-[45ch] text-lg ">{project.overview}</p>
+                  </div>
+                  <div className="flex flex-col space-y-3">
+                    <h3 className="capitalize text-2xl sm:text-4xl">
+                      Tech-stacks
+                    </h3>
+                    <ul className="flex gap-2 flex-wrap text-white text-[1rem]">
+                      {project.techStacks.map((techStack: string) => (
+                        <li className="bg-black px-2 h-6 flex items-center">
+                          {techStack}
+                        </li>
+                      ))}
+
+                      {/* <li className="bg-black px-2 h-6 flex items-center">
+                        Formik
+                      </li>
+                      <li className="bg-black px-2 h-6 flex items-center">
+                        React-router
+                      </li> */}
+                    </ul>
+                  </div>
+                </>
+              );
+            }
+          })}
         </div>
       </section>
     </>
