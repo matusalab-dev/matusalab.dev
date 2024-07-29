11760781077d437a2f6efc73f145f57546ab34a7
matusala <mtslabrh97@gmail.com>
2024-07-29 18:23:58 +0300

    integrate google tag manager to my portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const satoshi = localFont({
   src: [
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${satoshi.className} antialiased w-full max-w-[87.5rem] mx-auto px-7 md:px-12`}
       >
+        <GoogleTagManager gtmId="G-LGSF7FLRXD" />
         {children}
         <Analytics />
         <SpeedInsights />
