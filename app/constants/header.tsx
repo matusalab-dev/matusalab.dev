f8d80b5a999dfb400c69313dde3776877d168c18
matusala <mtslabrh97@gmail.com>
2024-12-25 17:39:30 +0300

    update: on my hero section

--- app/constants/header.tsx
+++ app/constants/header.tsx
@@ -11,7 +11,7 @@ export const HEADER_COPYRIGHT: HeaderCopyTypes[] = [
     description:
       // "Hi there! I’m Matusala Abrha, a passionate who loves building things with code. As I've grown as a developer, I've dedicated myself to personally developing web applications that meet high standards and provide exceptional user experiences.",
       // "Hi there! I’m Matusala Abrha, an Electrical and Computer Engineering graduate with hands-on experience, I have developed web solutions, such as an e-commerce furniture store. My skills include writing clean, thorough debugging, and writing technical documentation. I excel in collaborating with teams and staying updated with industry trends. My background and experience make me a strong fit for your software development position ",
-      "Hi, I’m Matusala Abrha, I’m a creative React Developer with a degree in Electrical and Computer Engineering and a passion for building meaningful digital experiences. I work with Shopify, using HTML, CSS, JavaScript, Alpine.js, and Liquid to create custom themes and unique features that meet my clients’ needs. My goal is to design responsive, user-friendly interfaces that make a real impact. Let’s bring your ideas to life together!",
+      "Hi, I’m Matusala Abrha, I’m a creative React Developer with a degree in Electrical and Computer Engineering and a passion for building meaningful digital experiences. I work with Shopify, using HTML, CSS, JavaScript, Alpine.js, and Liquid to create custom shopify themes and unique features that meet my clients’ needs. My goal is to design responsive, user-friendly and functional web apps that make a real impact. Let’s bring your ideas to life together!",
   },
   {
     tags: ["React", "Next.js"],
