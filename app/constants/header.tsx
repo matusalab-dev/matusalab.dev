96858d847dd39a8f6ec0b31801e308f492d8aeb6
matusala <mtslabrh97@gmail.com>
2025-05-05 13:08:32 +0300

    update the hero-section

--- app/constants/header.tsx
+++ app/constants/header.tsx
@@ -9,7 +9,7 @@ export const HEADER_COPYRIGHT: HeaderCopyTypes[] = [
     tags: ["React", "Shopify", "Next.js", "Full-stack"],
     headline: ["Front-End", "Shopify", "Next.js"],
     description:
-      "Hi there, I’m Matusala Abrha, Full-Stack Next.js Engineer based in Ethiopia with hands-on experience in React, Next.js, TypeScript, and Firebase. Currently building scalable UIs at Blih Marketing & Communications plc, and delivering Shopify solutions with Liquid, GraphQL, and Alpine.js. Used Leaflet.js for geospatial features in Taxi-Tera and Chart.js for dashboards in the MHUR LMS. Skilled in Node.js, REST APIs, and performance optimization. Eager to build impactful, AI-powered platforms like Addis Insights.",
+      "Hi there, I’m Matusala Abrha, Full-Stack Next.js Engineer based in Ethiopia with hands-on experience in React, Next.js, TypeScript, and Firebase. Currently building scalable UIs at Blih Marketing & Communications plc, and delivering Shopify solutions with Liquid, GraphQL, and Alpine.js. Used Leaflet.js for geospatial features in Taxi-Tera and Chart.js for dashboards in the MHUR LMS. Skilled in Node.js, REST APIs, and performance optimization. Eager to build impactful, AI-powered platforms.",
   },
   {
     tags: ["React", "Next.js"],
